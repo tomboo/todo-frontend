07a19cf1c53881a1ebe201bc783fd15f4288787c
tomboo <tom.booster@gmail.com>
2019-11-19 06:55:00 -0800

    Chapter 4: Todo React Front-end

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-// src/App.js
 import React, { Component } from 'react';
-
-const list = [
-  {
-    "id":1,
-    "title":"1st todo", "body":"Learn Django properly."
-  }, {
-    "id":2, "title":"Second item",
-    "body":"Learn Python."
-  }, {
-    "id":3,
-    "title":"Learn HTTP", "body":"It's important."
-  }
-]
+import axios from 'axios'; 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { list };
+  state = {
+    todos: []
+  };
+
+  // new
+  componentDidMount() {
+    this.getTodos();
+  }
+
+  // new
+  getTodos() {
+    axios
+      .get('http://127.0.0.1:8000/api/')
+      .then(res => {
+        this.setState({ todos: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render() {
     return (
       <div>
-        {this.state.list.map(item => (
+        {this.state.todos.map(item => (
           <div key={item.id}>
             <h1>{item.title}</h1>
-            <p>{item.body}</p>
+            <span>{item.body}</span>
           </div>
         ))}
-      </div> );
-    }
+      </div>
+    );
   }
+}
 
 export default App;
